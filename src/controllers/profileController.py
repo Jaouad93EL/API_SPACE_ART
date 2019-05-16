f27c810143d1e2fbbbca735904bfe8353cbae55a
Jaouad93EL <elhormij@gmail.com>
2019-05-16 12:54:26 +0200

    profile

--- src/controllers/profileController.py
+++ src/controllers/profileController.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import request, Blueprint, g
 from ..models.ProfileModel import ProfileModel, ProfileSchema
 from ..models.UserModel import UserModel, UserSchema
@@ -64,6 +66,25 @@ def update_picture():
     url = google.store_in_google("audio_space_art", str(g.user.get('id')), picture_storage)
     profile.picture_profile(url, picture_storage.filename)
     return custom_response({'successful': {'url': urllib.parse.unquote(profile.picture_url)}}, 200)
+
+
+
+@profile_api.route('/update_pic_ban', methods=['PUT'])
+@Auth.auth_required
+def update_pic_ban():
+    picture_storage = request.files.get('picture')
+    banner_storage = request.files.get('banner')
+    picture_url = None
+    banner_url = None
+    if picture_storage: picture_storage = request.put(request.url_root + 'api/profil/update_picture', files={'picture': picture_storage}, headers=request.headers)
+    if banner_storage: banner_storage = request.put(request.url_root + 'api/profil/update_banner', files={'banner': banner_storage}, headers=request.headers)
+    if picture_storage.status_code == 200: picture_url = json.dumps(picture_storage.text)
+    if banner_storage.status_code == 200: banner_url = json.dumps(banner_storage.text)
+    url = {
+        'picture_url': picture_url['succcessful']['url'],
+        'banner_url': banner_url['succcessful']['url']
+    }
+    return custom_response({'link': {url}}, 200)
 
 
 @profile_api.route('/get_my_profile', methods=['GET'])
