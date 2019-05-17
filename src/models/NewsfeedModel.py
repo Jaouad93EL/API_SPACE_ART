3f381f06879d16eacf021394a123a496196d3b11
Jaouad93EL <elhormij@gmail.com>
2019-05-17 10:49:38 +0200

    profile

---
+++ src/models/NewsfeedModel.py
@@ -0,0 +1,44 @@
+import datetime
+from marshmallow import fields, Schema
+from . import db
+
+class NewsfeedModel(db.Model):
+    __tablename__ = 'newsfeed'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(128), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
+
+    def __init__(self, data, user_id):
+        self.text = data.get('text')
+        self.user_id = user_id
+        self.created_at = datetime.datetime.utcnow()
+        self.modified_at = datetime.datetime.utcnow()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data.items():
+            setattr(self, key, item)
+        self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
+
+    def __repr(self):
+        return '<id {}>'.format(self.id)
+
+    def get_id(self):
+        return self.id
+
+class NewsfeedSchema(Schema):
+    id = fields.Int(dump_only=True)
+    text = fields.Str(required=True)
+    user_id = fields.Int(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
