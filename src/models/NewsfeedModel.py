340d44bc154faf8dcce0a9737a28ca7973217ea4
Jaouad93EL <elhormij@gmail.com>
2019-05-18 18:55:15 +0200

    profile

--- src/models/NewsfeedModel.py
+++ src/models/NewsfeedModel.py
@@ -6,14 +6,14 @@ class NewsfeedModel(db.Model):
     __tablename__ = 'newsfeed'
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(128), nullable=False)
-    post_id = db.Column(db.Integer, nullable=False)
+    parent_id = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
 
-    def __init__(self, type, post_id, user_id):
+    def __init__(self, type, parent_id, user_id):
         self.type = type
-        self.post_id = post_id
+        self.parent_id = parent_id
         self.user_id = user_id
         self.created_at = datetime.datetime.utcnow()
         self.modified_at = datetime.datetime.utcnow()
@@ -41,7 +41,7 @@ class NewsfeedModel(db.Model):
 class NewsfeedSchema(Schema):
     id = fields.Int(dump_only=True)
     type = fields.Str(required=True)
-    post_id = fields.Int(required=True)
+    parent_id = fields.Int(required=True)
     user_id = fields.Int(required=True)
     created_at = fields.DateTime(dump_only=True)
     modified_at = fields.DateTime(dump_only=True)
