8f15392858455654471bfcfba3ca0dc0e7ff820b
Jaouad93EL <elhormij@gmail.com>
2019-06-06 10:59:34 +0200

    profile

--- src/controllers/postController.py
+++ src/controllers/postController.py
@@ -1,11 +1,7 @@
 from flask import request, Blueprint, g
-from .mailController import login_success,randomString
-from ..models.UserModel import UserModel
 from ..models.PostModel import PostModel, PostSchema
-from ..models.RevokedTokenModel import RevokedTokenModel
 from src.jsonResponse import custom_response
 from ..shared.Authentication import Auth
-from ..Google_storage import google
 
 
 user_api = Blueprint('users', __name__)
