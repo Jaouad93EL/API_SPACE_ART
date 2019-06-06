51d44318d84b3f35972417497e7d900e43c98641
Jaouad93EL <elhormij@gmail.com>
2019-06-06 10:59:05 +0200

    profile

--- src/controllers/postController.py
+++ src/controllers/postController.py
@@ -12,6 +12,12 @@ user_api = Blueprint('users', __name__)
 post_schema = PostSchema()
 
 @user_api.route('/create_post', methods=['POST'])
-def create():
+@Auth.auth_required
+def create_post():
     req_data = request.get_json()
     data, error = post_schema.load(req_data)
+    if error:
+        return custom_response(error, 400)
+    post = PostModel(data, g.user.get('id'))
+    post.save()
+    return custom_response({'success': 'Post created'}, 200)
